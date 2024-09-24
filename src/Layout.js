a165a2aaea11aff47242e2b62430cb6ee10d9f40
BenChris090 <benedictchrstn@gmail.com>
2024-09-24 11:32:00 +0100

    CONTENT COMMIT

--- src/Layout.js
+++ src/Layout.js
@@ -27,8 +27,8 @@ const Layout = ({ children, pageTitle, pageIcon: PageIcon }) => {
     <div className="flex w-full overflow-y-hidden">
       {/* Mobile Navigation */}
       <div className={`z-0 fixed overflow-y-auto bg-white h-screen w-full shadow-xl ${mNav ? 'block' : 'hidden'}`} id="mNav-options">
-        <div className="flex items-center justify-between w-full p-3">
-          <Link to="/" className="flex items-center w-full m-auto pl-3 lg:max-h-16">
+        <div className="flex items-center justify-around w-full p-3 py-4">
+          <Link to="/" className="flex items-center w-full m-auto lg:max-h-16">
             <span className="self-center PX-2 text-xl font-semibold whitespace-nowrap text-cyan-900 lg:text-4xl">OCTILLION WHALES</span>
           </Link>
           <FaTimes className="text-2xl m-2 text-cyan-900" onClick={toggleNav} />
